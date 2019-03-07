376cf2ecf9ca2b20f15e5cefb45fb115bfd06b7e
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-03-07 14:33:23 +0300

    Small adoptation to make deployment work

--- pastel_cloud/settings.py
+++ pastel_cloud/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -168,3 +170,5 @@ SITE_ID = 1
 REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'pastel_registration.views.PastelRegisterSerializer'
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
